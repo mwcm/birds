de6ad59de7ad81b5373cfe7eb9a37d546547d644
morgan <mwcmitchell@gmail.com>
2020-06-27 18:00:45 -0400

    fly towards center working

--- boids.js
+++ boids.js
@@ -14,22 +14,34 @@ document.body.appendChild(renderer.domElement);
 camera.position.z = 350;
 
 var Boid = function () {
-	this.position = new THREE.Vector3();
+	this.geometry = new THREE.ConeGeometry(1, 4, 5.3);
+	this.material = new THREE.MeshBasicMaterial({color: 0xffff00});
+	this.mesh = new THREE.Mesh(this.geometry,  this.material);
+
 	this.velocity = new THREE.Vector3();
 	this.acceleration = new THREE.Vector3();
 }
 
-function drawBoid() {
-	var geometry = new THREE.ConeGeometry(1, 4, 5.3);
-	var material = new THREE.MeshBasicMaterial({color: 0xffff00});
-	var boid = new THREE.Mesh(geometry,  material);
-	boid.position.x = Math.random() * width - width/2;
-	boid.position.y = Math.random() * height - height/2;
-	boid.position.z = Math.random() * depth - depth/2;
-	scene.add(boid);
-	boids.push(boid)
+function calculateCenter(boid, boids) {
+	var c = new THREE.Vector3();
+	for (var i = 0; i < numBoids; i++){
+		c.add(boids[i].mesh.position);
+	}
+	// assuming supplied boid is part of the boids group
+	c = c.sub(boid.mesh.position)
+	c = c.divideScalar(boids.length)
+	console.log(c)
+	return c
 }
 
+function drawBoid() {
+	var boid = new Boid()
+	boid.mesh.position.x = Math.random() * width - width/2;
+	boid.mesh.position.y = Math.random() * height - height/2;
+	boid.mesh.position.z = Math.random() * depth - depth/2;
+	scene.add(boid.mesh);
+	boids.push(boid)
+}
 
 function drawBoids() {
 	for(var i = 0; i < numBoids; i ++){
@@ -37,10 +49,18 @@ function drawBoids() {
 	}
 }
 
+function flyTowardsCentre(boid, boids) {
+	boidCenter = calculateCenter(boid, boids)
+	boidCenter = boidCenter.sub(boid.mesh.position)
+	boidCenter = boidCenter.divideScalar(100) // move 1% distance to center
+	return boidCenter
+}
+
 function move() {
 	boids.forEach(function(boid, index) {
-		boid.position.x += 0.1
-		boid.position.y += 0.1
+		v1 = flyTowardsCentre(boid, boids)
+		boid.velocity = boid.velocity.add(v1)
+	  boid.mesh.posittion = boid.mesh.position.add(boid.velocity)
 	})
 }
 
@@ -51,4 +71,4 @@ function animate() {
 }
 
 drawBoids()
-animate();
+animate()
