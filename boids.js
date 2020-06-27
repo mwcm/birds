b54fd40280857e75c14b21044db7ead5780ee21a
morgan <mwcmitchell@gmail.com>
2020-06-27 16:33:38 -0400

    floating cones

--- boids.js
+++ boids.js
@@ -1,19 +1,24 @@
 var _boids = [];
+var _numBoids = 100;
 var _scene = new THREE.Scene();
 var _camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var _renderer = new THREE.WebGLRenderer();
 _renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(_renderer.domElement);
-_camera.position.z = 100;
+_camera.position.z = 500;
 
-function draw_boid() {
-	var geometry = new THREE.ConeGeometry(5, 20, 32);
+function drawBoid() {
+	var geometry = new THREE.ConeGeometry(1, 4, 5.3);
 	var material = new THREE.MeshBasicMaterial({color: 0xffff00});
-	var cone = new THREE.Mesh(geometry,  material);
-	// cone.position.x = 0
-	// cone.position.y = 0
-	_scene.add(cone);
-	_boids.push(cone)
+	var boid = new THREE.Mesh(geometry,  material);
+	boid.position.x = Math.random() * 500 - 250;
+	boid.position.y = Math.random() * 500 - 250;
+	boid.position.z = Math.random() * 500 - 250;
+	// boid.velocity.x = Math.random() * 2 - 1;
+	// boid.velocity.y = Math.random() * 2 - 1;
+	// boid.velocity.z = Math.random() * 2 - 1;
+	_scene.add(boid);
+	_boids.push(boid)
 }
 
 
@@ -32,5 +37,11 @@ function animate() {
 	move()
 }
 
-draw_boid()
+function drawAll() {
+	for(var i = 0; i < _numBoids; i ++){
+		drawBoid()
+	}
+}
+
+drawAll()
 animate();
